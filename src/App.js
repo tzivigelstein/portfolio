c0c43d921eec13b97e35f9e143fb3106c33a8736
Tzivi Gelstein <gelsteintzivisantiago@gmail.com>
2021-11-29 11:59:16 -0300

    Add app state wrapping

--- src/App.js
+++ src/App.js
@@ -1,15 +1,19 @@
 import React from 'react'
+import AppState from './context/app/appState'
+import Container from './components/Container'
 import Header from './components/Header/Header'
 import Sections from './components/Sections/Sections'
 import Footer from './components/Footer/Footer'
 
 const App = () => {
   return (
-    <main>
-      <Header />
-      <Sections />
-      <Footer />
-    </main>
+    <AppState>
+      <Container>
+        <Header />
+        <Sections />
+        <Footer />
+      </Container>
+    </AppState>
   )
 }
 
