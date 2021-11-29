2c32beede4720cf7c76d6eaaf359564fdd86ad60
Tzivi Gelstein <gelsteintzivisantiago@gmail.com>
2021-11-29 12:00:28 -0300

    Add Sun and Moon icons

--- src/components/UICons.js
+++ src/components/UICons.js
@@ -48,3 +48,36 @@ export const TimesIcon = props => (
     <path d="M18 6L6 18M6 6l12 12" />
   </svg>
 )
+
+export const SunIcon = props => (
+  <svg
+    width={24}
+    height={24}
+    stroke="currentColor"
+    strokeWidth={2}
+    fill="none"
+    strokeLinecap="round"
+    strokeLinejoin="round"
+    className="prefix__css-i6dzq1"
+    {...props}
+  >
+    <circle cx={12} cy={12} r={5} />
+    <path d="M12 1v2M12 21v2M4.22 4.22l1.42 1.42M18.36 18.36l1.42 1.42M1 12h2M21 12h2M4.22 19.78l1.42-1.42M18.36 5.64l1.42-1.42" />
+  </svg>
+)
+
+export const MoonIcon = props => (
+  <svg
+    width={24}
+    height={24}
+    stroke="currentColor"
+    strokeWidth={2}
+    fill="none"
+    strokeLinecap="round"
+    strokeLinejoin="round"
+    className="prefix__css-i6dzq1"
+    {...props}
+  >
+    <path d="M21 12.79A9 9 0 1111.21 3 7 7 0 0021 12.79z" />
+  </svg>
+)
