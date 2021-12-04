d892f5094d51975e8bd4f1482e963828a374321e
Tzivi Gelstein <gelsteintzivisantiago@gmail.com>
2021-12-04 13:47:16 -0300

    Create HeaderButtonLink component

---
+++ src/components/HeaderButton/index.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import styles from './index.module.css'
+
+const HeaderLinkButton = ({ text, link }) => {
+  return (
+    <a className={styles.headerLinkButton} href={link} target="_blank" rel="noopener noreferrer">
+      {text}
+    </a>
+  )
+}
+
+export default HeaderLinkButton
