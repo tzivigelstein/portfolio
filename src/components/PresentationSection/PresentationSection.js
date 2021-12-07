34cf40ab02ebac20d109e589dd92add51ad2fa7a
Tzivi Gelstein <gelsteintzivisantiago@gmail.com>
2021-12-07 12:21:52 -0300

    Add Presentation Section texts translation

--- src/components/PresentationSection/PresentationSection.js
+++ src/components/PresentationSection/PresentationSection.js
@@ -1,15 +1,15 @@
 import React from 'react'
+import { useTranslation } from 'react-i18next'
 import styles from './presentation.module.css'
 
 const PresentationSection = () => {
+  const [t] = useTranslation('', { useSuspense: false })
+
   return (
     <section className={styles.container}>
       <div className={styles.info}>
-        <h2 className={styles.title}>My projects</h2>
-        <p className={styles.text}>
-          Over the course of 2 years I have worked on different personal projects such as web applications, mobile
-          applications and web designs, in order to practice the skills learned during my learning.
-        </p>
+        <h2 className={styles.title}>{t('presentationSectionTitle')}</h2>
+        <p className={styles.text}>{t('presentationSectionText')}</p>
       </div>
       <div className={styles.imageContainer}>
         <img className={styles.image} loading="lazy" src="./images/previewed.webp" alt="Projects preview" />
