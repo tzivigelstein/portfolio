eefb1d262b9be7ad61ba67d729ee27fe027f5d28
Tzivi Gelstein <gelsteintzivisantiago@gmail.com>
2021-12-07 12:21:33 -0300

    Add Projects Section texts translation

--- src/components/ProjectsSection/ProjectsSection.js
+++ src/components/ProjectsSection/ProjectsSection.js
@@ -1,16 +1,19 @@
 import React from 'react'
 import styles from './projectsection.module.css'
+import { useTranslation } from 'react-i18next'
 import projects from '../../projects'
 import Card from '../Card'
 
 const ProjectsSection = () => {
+  const [t] = useTranslation('', { useSuspense: false })
+
   const webs = projects.filter(({ category }) => category === 'web')
   const apps = projects.filter(({ category }) => category === 'app')
 
   return (
     <section className={styles.container}>
       <section className={styles.secondarySection}>
-        <h2 className={styles.sectionTitle}>Web Projects</h2>
+        <h2 className={styles.sectionTitle}>{t('projectsSectionWebTitle')}</h2>
         <ul className={styles.projectList}>
           {webs.map(project => (
             <Card key={project.id} project={project} />
@@ -18,7 +21,7 @@ const ProjectsSection = () => {
         </ul>
       </section>
       <section className={styles.secondarySection}>
-        <h2 className={styles.sectionTitle}>App Projects</h2>
+        <h2 className={styles.sectionTitle}>{t('projectsSectionAppTitle')}</h2>
         <ul className={styles.projectList}>
           {apps.map(project => (
             <Card key={project.id} project={project} />
