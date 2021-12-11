6bca7d0bd555c5d3652f4e8def0e37b225b4a736
Tzivi Gelstein <gelsteintzivisantiago@gmail.com>
2021-12-11 19:42:07 -0300

    Add translation button and button styling

--- src/components/ControlBar/index.js
+++ src/components/ControlBar/index.js
@@ -1,11 +1,13 @@
 import React, { useState, useEffect } from 'react'
 import styles from './index.module.css'
-import { SunIcon, MoonIcon } from '../UICons'
+import { SunIcon, MoonIcon, LanguageIcon } from '../UICons'
 import useApp from '../../hooks/useApp'
+import { useTranslation } from 'react-i18next'
 
 const ControlBar = () => {
   const { theme, setTheme } = useApp()
   const [isChecked, setChecked] = useState(theme === 'dark')
+  const [t, i18n] = useTranslation('', { useSuspense: false })
 
   useEffect(() => {
     window.addEventListener('keydown', handleKeyDown)
@@ -25,21 +27,35 @@ const ControlBar = () => {
     setTheme(theme === 'dark' ? 'light' : 'dark')
   }
 
+  function handleLanguageChange() {
+    i18n.changeLanguage(i18n.language === 'en' ? 'es' : 'en')
+  }
+
   return (
     <div className={styles.controlBarContainer}>
-      <input
-        id="checkbox"
-        type="checkbox"
-        className={styles.checkBox}
-        checked={isChecked}
-        onChange={handleCheckboxChange}
-      />
-      <label htmlFor="checkbox" className={styles.toggle}>
-        <div className={styles.toggleIconContainer}>
-          <SunIcon />
-          <MoonIcon className={styles.moonIcon} />
+      <div>
+        <input
+          id="checkbox"
+          type="checkbox"
+          className={styles.checkBox}
+          checked={isChecked}
+          onChange={handleCheckboxChange}
+        />
+        <label htmlFor="checkbox" className={styles.toggle}>
+          <div className={styles.toggleIconContainer}>
+            <SunIcon />
+            <MoonIcon className={styles.moonIcon} />
+          </div>
+        </label>
+      </div>
+      <div onClick={handleLanguageChange} className={styles.barToggle}>
+        <div className={styles.iconContainer}>
+          <LanguageIcon className={styles.languageIcon} />
+          <div className={styles.languageIndicator}>
+            <span className={styles.languageIndicatorText}>{i18n.language}</span>
+          </div>
         </div>
-      </label>
+      </div>
     </div>
   )
 }
