c46c657313b5c76b1aac22419af1ca22fe1303cb
Tzivi Gelstein <gelsteintzivisantiago@gmail.com>
2021-12-21 20:10:29 -0300

    Delete unused code

--- src/context/app/appReducer.js
+++ src/context/app/appReducer.js
@@ -1,13 +1,7 @@
-import { SET_THEME } from './types'
-
 const appReducer = (state, { type, payload }) => {
   switch (type) {
-    case SET_THEME:
-      window.localStorage.setItem('theme', JSON.stringify(payload))
-      return {
-        ...state,
-        theme: payload,
-      }
+    default:
+      return { ...state }
   }
 }
 
