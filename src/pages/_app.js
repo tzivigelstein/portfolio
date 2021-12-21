ebbf7e1d32b416f38572a0577a1d94579c5eb381
Tzivi Gelstein <gelsteintzivisantiago@gmail.com>
2021-12-20 21:04:18 -0300

    Populate app Head

---
+++ src/pages/_app.js
@@ -0,0 +1,43 @@
+import Head from 'next/head'
+import '../index.css'
+
+const MyApp = ({ Component, pageProps }) => (
+  <>
+    <Head>
+      <meta charset="utf-8" />
+      <link rel="icon" type="image/png" href="/favicon.png" />
+      <meta name="viewport" content="width=device-width, initial-scale=1" />
+      <meta name="theme-color" content="#6ec2f7" />
+      //Primary metatags
+      <title>Tzivi | Portfolio</title>
+      <meta name="title" content="Tzivi | Portfolio" />
+      <meta
+        name="description"
+        content="I'm Tzivi and this is my portfolio 👨‍💻. Come on in and find out more about me 🤗."
+      />
+      //Open Graph / Facebook
+      <meta property="og:type" content="website" />
+      <meta property="og:url" content="https://tzivi.vercel.app/" />
+      <meta property="og:title" content="Tzivi | Portfolio" />
+      <meta
+        property="og:description"
+        content="I'm Tzivi and this is my portfolio 👨‍💻. Come on in and find out more about me 🤗."
+      />
+      <meta property="og:image" content="./images/Cover.webp" />
+      //Twitter
+      <meta property="twitter:card" content="summary_large_image" />
+      <meta property="twitter:url" content="https://tzivi.vercel.app/" />
+      <meta property="twitter:title" content="Tzivi | Portfolio" />
+      <meta
+        property="twitter:description"
+        content="I'm Tzivi and this is my portfolio 👨‍💻. Come on in and find out more about me 🤗."
+      />
+      <meta property="twitter:image" content="/images/Cover.webp" />
+      <link rel="apple-touch-icon" href="/favicon.png" />
+      <link rel="manifest" href="/manifest.json" />
+    </Head>
+    <Component {...pageProps} />
+  </>
+)
+
+export default MyApp
