09699494601a38d00f5079c1269a3736fb77fa5c
Tzivi Gelstein <gelsteintzivisantiago@gmail.com>
2021-12-22 17:35:56 -0300

    Fix attribute casing

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -5,7 +5,7 @@ import { ThemeProvider } from 'next-themes'
 const MyApp = ({ Component, pageProps }) => (
   <>
     <Head>
-      <meta charset="utf-8" />
+      <meta charSet="utf-8" />
       <link rel="icon" type="image/png" href="/favicon.png" />
       <meta name="viewport" content="width=device-width, initial-scale=1" />
       <meta name="theme-color" content="#6ec2f7" />
