9d42a8da537f56ce2bad003dcab30351a8599dd9
Tzivi Gelstein <gelsteintzivisantiago@gmail.com>
2021-12-26 15:45:40 -0300

    Update code styling

--- src/components/TechIcon/TechIcon.js
+++ src/components/TechIcon/TechIcon.js
@@ -3,12 +3,7 @@ import styles from './techicon.module.css'
 const TechIcon = ({ children, backgroundColor, name, link }) => {
   return (
     <a href={link} target="_blank" rel="noopener noreferrer" style={{ position: 'relative' }}>
-      <div
-        style={{
-          backgroundColor,
-        }}
-        className={styles.skill}
-      >
+      <div style={{ backgroundColor }} className={styles.skill}>
         {children}
       </div>
       <div translate="no" className={styles.skillTooltip}>
