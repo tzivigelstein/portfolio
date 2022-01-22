ad46dcfb8c9dfea0754046d36c17eb0998f08335
Tzivi Gelstein <gelsteintzivisantiago@gmail.com>
2022-01-22 12:36:58 -0300

    Delete global state wrapper

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,4 +1,3 @@
-import AppState from '../context/app/appState'
 import Container from '../components/Container'
 import Header from '../components/Header/Header'
 import Sections from '../components/Sections/Sections'
@@ -7,14 +6,12 @@ import ControlBar from '../components/ControlBar'
 
 const Home = () => {
   return (
-    <AppState>
-      <Container>
-        <ControlBar />
-        <Header />
-        <Sections />
-        <Footer />
-      </Container>
-    </AppState>
+    <Container>
+      <ControlBar />
+      <Header />
+      <Sections />
+      <Footer />
+    </Container>
   )
 }
 
