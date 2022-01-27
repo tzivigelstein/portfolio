2921845aaff0c51b38e68339dc85208b3b7d16f5
Tzivi Gelstein <gelsteintzivisantiago@gmail.com>
2022-01-27 15:03:51 -0300

    Update data structure

--- src/components/TechIcon/TechIcon.js
+++ src/components/TechIcon/TechIcon.js
@@ -1,12 +1,12 @@
 import styles from './techicon.module.css'
 
-const TechIcon = ({ children, backgroundColor, name, link }) => {
+const TechIcon = ({ children, name, link, styles: iconStyles }) => {
   return (
-    <a href={link} target='_blank' rel='noopener noreferrer' style={{ position: 'relative' }}>
-      <div style={{ backgroundColor }} className={styles.skill}>
+    <a href={link} target="_blank" rel="noopener noreferrer" style={{ position: 'relative' }}>
+      <div style={iconStyles} className={styles.skill}>
         {children}
       </div>
-      <div translate='no' className={styles.skillTooltip}>
+      <div translate="no" className={styles.skillTooltip}>
         <span>{name}</span>
       </div>
     </a>
