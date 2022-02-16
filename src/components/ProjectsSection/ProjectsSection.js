fcad734ff756455990d89de9f288be4a9034f131
Tzivi Gelstein <gelsteintzivisantiago@gmail.com>
2022-02-16 10:43:28 -0300

    Add Jumbo to projects

--- src/components/ProjectsSection/ProjectsSection.js
+++ src/components/ProjectsSection/ProjectsSection.js
@@ -1,12 +1,14 @@
 import styles from './projectsection.module.css'
 import projects from '../../projects'
 import Card from '../Card'
+import JumboCard from '../JumboCard'
 import useTranslation from 'next-translate/useTranslation'
 
 const ProjectsSection = () => {
   const { t } = useTranslation()
 
   const webs = projects.filter(({ category }) => category === 'web')
+  const jumbo = webs.shift()
   const apps = projects.filter(({ category }) => category === 'app')
 
   return (
@@ -14,6 +16,7 @@ const ProjectsSection = () => {
       <section className={styles.secondarySection}>
         <h2 className={styles.sectionTitle}>{t('common:projectsSectionWebTitle')}</h2>
         <ul className={styles.projectList}>
+          <JumboCard jumbo key={jumbo.id} project={jumbo} />
           {webs.map(project => (
             <Card key={project.id} project={project} />
           ))}
