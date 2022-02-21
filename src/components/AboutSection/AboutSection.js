35d704d4143714f7461ae99b78a568aad24f4b6c
Tzivi Gelstein <gelsteintzivisantiago@gmail.com>
2022-02-21 15:41:09 -0300

    Add recommended creators

--- src/components/AboutSection/AboutSection.js
+++ src/components/AboutSection/AboutSection.js
@@ -2,6 +2,10 @@ import styles from './aboutsection.module.css'
 import TechIcon from '../TechIcon/TechIcon'
 import technologies from '../technologies'
 import useTranslation from 'next-translate/useTranslation'
+import { H2, H3, H5 } from '../Heading'
+import CREATORS from '../../creators'
+import Creator from '../Creator'
+import { ParagraphText } from '../Text'
 
 const AboutSection = () => {
   const { t } = useTranslation()
@@ -15,7 +19,7 @@ const AboutSection = () => {
             <h3 className={styles.subtitle}>
               {t('common:experienceTitle')} <span className={styles.icon}>👨‍🔬</span>
             </h3>
-            <p className={styles.text}>{t('common:experienceText')}</p>
+            <ParagraphText>{t('common:experienceText')}</ParagraphText>
           </div>
           <div>
             <h3 className={styles.subtitle}>{t('common:goalsTitle')} ✍</h3>
@@ -54,6 +58,38 @@ const AboutSection = () => {
             </div>
           </div>
         </div>
+      </div>
+      <div className={styles.storyContainer}>
+        <H3>My story</H3>
+        <div className={styles.textContainer}>
+          <ParagraphText>
+            I am Tzivi Gelstein and I was born at Béer Sheva, Israel. I grew up there until the age of 3, then my family
+            and I moved back to Argentina where we live happily.
+          </ParagraphText>
+          <ParagraphText>
+            I’ve been always a tech passionate, since I was young I enjoyed using the computer, gaming mainly 😅. But
+            for me, the machine I had in front was in fact a whole mystery.
+          </ParagraphText>
+          <ParagraphText>
+            A short time later, I started digging into a computer building, *What is a processor? What is a Motherboard?
+            Why my data held on my hard disk? These are the questions I had that nobody could answer. Nobody but the
+            internet.
+          </ParagraphText>
+          <ParagraphText>
+            It was a complete year learning about computers, all about them. From the transistors inside all the
+            components to the software. I learnt how to install an O.S. and how to configure it, I learnt (superficially
+            😁) what were the drivers.
+          </ParagraphText>
+        </div>
+      </div>
+      <div>
+        <H2>{t('common:recommendationsTitle')}</H2>
+        <H5>{t('common:recommendationsSubTitle')}</H5>
+        <ul className={styles.recommendationsContainer}>
+          {CREATORS.map(creator => (
+            <Creator key={creator.id} creator={creator} />
+          ))}
+        </ul>
       </div>
     </section>
   )
---
+++ src/creators.js
@@ -0,0 +1,56 @@
+export default [
+  {
+    id: 0,
+    name: 'Miguel Ángel Durán',
+    image: 'https://pbs.twimg.com/profile_images/1083714591752941568/Q7LnIANs_400x400.jpg',
+    website: 'https://www.midu.dev'
+  },
+  {
+    id: 1,
+    name: 'Oscar Barajas',
+    image: 'https://arepa.s3.amazonaws.com/oscar-barajas-tavares.png',
+    website: 'https://gndx.dev'
+  },
+  {
+    id: 2,
+    name: 'Daniel Shiffman',
+    image: 'https://tisch.nyu.edu/content/dam/tisch/itp/GeneralPics/DanielShiffman1.png',
+    website: 'https://shiffman.net'
+  },
+  {
+    id: 3,
+    name: 'Freddy Vega',
+    image: 'https://freddyvega.com/content/images/2020/08/freddy-vega-2.jpg',
+    website: 'https://freddyvega.com'
+  },
+  {
+    id: 4,
+    name: 'Antonio Sarosi',
+    image: 'https://avatars.githubusercontent.com/u/54243067?v=4',
+    website: 'https://github.com/antoniosarosi'
+  },
+  {
+    id: 5,
+    name: 'Fireship',
+    image: 'https://yt3.ggpht.com/ytc/AKedOLTcIl6kKt3lEPJEySUf_hpHiKDKiFeo9eWPReLysQ=s900-c-k-c0x00ffffff-no-rj',
+    website: 'https://fireship.io/'
+  },
+  {
+    id: 6,
+    name: 'Los apuntes de Majo',
+    image: 'https://avatars.githubusercontent.com/u/55170175?v=4',
+    website: 'https://losapuntesdemajo.vercel.app/'
+  },
+  {
+    id: 7,
+    name: 'Juan Jose Ortiz',
+    image: 'https://img-c.udemycdn.com/user/200_H/13799158_f1d1_3.jpg',
+    website: 'https://github.com/juanortiz10'
+  },
+  {
+    id: 6,
+    name: 'Kevin Powell',
+    image: 'https://d33wubrfki0l68.cloudfront.net/554ca802d12f9823e5449ae62bc6bc2775c5c67a/a52fd/img/f21b00ed-600.avif',
+    website: 'https://www.kevinpowell.co/'
+  }
+]
