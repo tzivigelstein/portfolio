3462f33d5646ce2599d369abf11889d9f1b6e851
Tzivi Gelstein <gelsteintzivisantiago@gmail.com>
2022-02-21 15:41:20 -0300

    Create creator component

---
+++ src/components/Creator/index.js
@@ -0,0 +1,14 @@
+import styles from './index.module.css'
+
+export default function Creator({ creator }) {
+  const { name, image, website } = creator
+
+  return (
+    <li className={styles.recommendationContainer}>
+      <a rel="noopener noreferrer" target="_blank" href={website}>
+        <img className={styles.recommendationImage} title={name} src={image} alt={name} />
+      </a>
+      <span className={styles.recommendationName}>{name}</span>
+    </li>
+  )
+}
