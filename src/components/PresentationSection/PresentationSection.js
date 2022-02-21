53f56a261417b1cb57a49a6977be18123cf2bb93
Tzivi Gelstein <gelsteintzivisantiago@gmail.com>
2022-02-21 15:43:11 -0300

    Remove old image and add copywrite

--- src/components/PresentationSection/PresentationSection.js
+++ src/components/PresentationSection/PresentationSection.js
@@ -1,18 +1,50 @@
 import styles from './presentation.module.css'
 import useTranslation from 'next-translate/useTranslation'
+import { ParagraphText } from '../Text'
+import { H2, H4 } from '../Heading'
+import { ChevronIcon } from '../UIICons'
 
 const PresentationSection = () => {
   const { t } = useTranslation()
 
   return (
     <section className={styles.container}>
-      <div className={styles.info}>
-        <h2 className={styles.title}>{t('common:presentationSectionTitle')}</h2>
-        <p className={styles.text}>{t('common:presentationSectionText')}</p>
+      <H2>{t('common:presentationSectionTitle')}</H2>
+      <div className={styles.information}>
+        <div>
+          <ParagraphText>{t('common:presentationSectionText')}</ParagraphText>
+          <H4>Con mi trabajo yo busco:</H4>
+          <ul className={styles.list}>
+            <li className={styles.listItem}>
+              <ChevronIcon />
+              <ParagraphText>
+                {/* {t('common:presentationSectionListItem1')} */}
+                <span className={styles.bold}>Optimizar</span> el uso de recursos para mejorar la experiencia del usuario
+                📊
+              </ParagraphText>
+            </li>
+            <li className={styles.listItem}>
+              <ChevronIcon />
+              <ParagraphText>
+                {/* {t('common:presentationSectionListItem1')} */}
+                <span className={styles.bold}>Crear</span> diseños y experiencias de usuario que sean amigables y fáciles
+                de utilizar 🎉
+              </ParagraphText>
+            </li>
+            <li className={styles.listItem}>
+              <ChevronIcon />
+              <ParagraphText>
+                {/* {t('common:presentationSectionListItem1')} */}
+                <span className={styles.bold}>Construir</span> aplicaciones escalables que generen un impacto positivo
+                en el usuario 🛠
+              </ParagraphText>
+            </li>
+          </ul>
+        </div>
       </div>
-      <div className={styles.imageContainer}>
-        <img className={styles.image} loading='lazy' src='./images/previewed.webp' alt='Projects preview' />
-      </div>
+      {/*  <div className={styles.imageContainer}>
+        <img className={styles.image} loading="lazy" src="./images/previewed.webp" alt="Projects preview" />
+      </div> */}
     </section>
   )
 }
