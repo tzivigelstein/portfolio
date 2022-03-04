6e8525a1a933ee1a32014239055bbe83c77efc80
Tzivi Gelstein <gelsteintzivisantiago@gmail.com>
2022-03-04 14:27:48 -0300

    Delete unused paragraphs

--- components/Story/index.js
+++ components/Story/index.js
@@ -2,47 +2,19 @@ import styles from './index.module.css'
 import { H3 } from '@components/Heading'
 import { ParagraphText, TextEmoji, TextItalic, TextLink, TextListItem } from '@components/Text'
 import useTranslation from 'next-translate/useTranslation'
+import { MDXRemote } from 'next-mdx-remote'
+import MDXComponents from '@components/MDXComponents'
 
-export default function Story () {
+export default function Story({ post }) {
   const { t } = useTranslation()
+
+  const { source } = post
 
   return (
     <div className={styles.storyContainer}>
       <H3>{t('common:storyTitle')}</H3>
       <div className={styles.textContainer}>
-        <ParagraphText>
-          {t('common:storyParagraph1Part1')}{' '}
-          <TextLink href='https://goo.gl/maps/mG91tRZRup2QB25Z6'>Béer Sheva, Israel</TextLink>
-          {t('common:storyParagraph1Part2')}
-        </ParagraphText>
-        <ParagraphText>
-          {t('common:storyParagraph2Part1')} <TextEmoji>😅</TextEmoji>
-          {t('common:storyParagraph2Part2')}
-        </ParagraphText>
-        <ParagraphText>
-          {t('common:storyParagraph3Part1')} <TextItalic>{t('common:storyParagraph3Part2')}</TextItalic>{' '}
-          <TextItalic>{t('common:storyParagraph3Part3')}</TextItalic>{' '}
-          <TextItalic>{t('common:storyParagraph3Part4')}</TextItalic> {t('common:storyParagraph3Part5')}
-        </ParagraphText>
-        <ParagraphText>{t('common:storyParagraph4Part1')}</ParagraphText>
-        <ParagraphText>{t('common:storyParagraph5Part1')}</ParagraphText>
-        <ParagraphText>{t('common:storyParagraph6Part1')}</ParagraphText>
-        <ParagraphText>
-          {t('common:storyParagraph7Part1')} <TextLink href='https://www.arduino.cc'>Arduino</TextLink>
-          {t('common:storyParagraph7Part2')}
-        </ParagraphText>
-        <ParagraphText>{t('common:storyParagraph8Part1')}</ParagraphText>
-        <ul className={styles.list}>
-          <li>
-            <TextListItem>{t('common:storyListItem1')}</TextListItem>
-          </li>
-          <li>
-            <TextListItem>{t('common:storyListItem2')}</TextListItem>
-          </li>
-          <li>
-            <TextListItem>{t('common:storyListItem3')}</TextListItem>
-          </li>
-        </ul>
+        <MDXRemote {...source} components={MDXComponents} />
       </div>
     </div>
   )
