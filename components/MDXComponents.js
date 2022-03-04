88a1a3f2910b4c59f7450cde0613e7cccf3437e0
Tzivi Gelstein <gelsteintzivisantiago@gmail.com>
2022-03-04 14:26:54 -0300

    Add MDX components

---
+++ components/MDXComponents.js
@@ -0,0 +1,10 @@
+import { ParagraphText, TextItalic, TextLink, TextEmoji, TextListItem, List } from '@components/Text'
+
+export default {
+  p: ParagraphText,
+  em: TextItalic,
+  TextLink,
+  TextEmoji,
+  TextListItem,
+  ul: List
+}
