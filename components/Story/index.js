82cc4d67d3773fc31232e0252f408c56ffbb01b5
Tzivi Gelstein <gelsteintzivisantiago@gmail.com>
2022-03-04 14:28:10 -0300

    Delete unused import

--- components/Story/index.js
+++ components/Story/index.js
@@ -1,6 +1,5 @@
 import styles from './index.module.css'
 import { H3 } from '@components/Heading'
-import { ParagraphText, TextEmoji, TextItalic, TextLink, TextListItem } from '@components/Text'
 import useTranslation from 'next-translate/useTranslation'
 import { MDXRemote } from 'next-mdx-remote'
 import MDXComponents from '@components/MDXComponents'
