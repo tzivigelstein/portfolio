05a8907334dad8fdc7e4f5781d1f5d7289cec19e
Tzivi Gelstein <gelsteintzivisantiago@gmail.com>
2022-04-12 11:00:09 -0300

    Add dynamic next images hosts

--- next.config.js
+++ next.config.js
@@ -1,3 +1,15 @@
 const nextTranslate = require('next-translate')
 
-module.exports = nextTranslate()
+module.exports = nextTranslate({
+  images: {
+    domains: [
+      'arepa.s3.amazonaws.com',
+      'tisch.nyu.edu',
+      'freddyvega.com',
+      'avatars.githubusercontent.com',
+      'yt3.ggpht.com',
+      'img-c.udemycdn.com',
+      'd33wubrfki0l68.cloudfront.net'
+    ]
+  }
+})
