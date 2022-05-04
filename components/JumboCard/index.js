8d1898552f64b50242455fbafa66cf1c5004eec9
Tzivi Gelstein <gelsteintzivisantiago@gmail.com>
2022-05-04 15:37:51 -0300

    Update Jumbo Card component

--- components/JumboCard/index.js
+++ components/JumboCard/index.js
@@ -1,5 +1,6 @@
 import styles from './index.module.css'
 import useTranslation from 'next-translate/useTranslation'
+import { ChevronIcon } from '@components/Icons'
 
 export default function JumboCard ({ project }) {
   const { theme, color, title, subtitle, image, mobileImage, repository, site, category } = project
@@ -8,34 +9,28 @@ export default function JumboCard ({ project }) {
 
   return (
     <article data-theme={theme} style={{ background: color }} className={styles.jumboCard}>
-      <a className={styles.projectLink} href={site} target='_blank' rel='noopener noreferrer'>
+      <a aria-label='main-link' className={styles.projectLink} href={site} target='_blank' rel='noopener noreferrer'>
         <header className={styles.cardHeader}>
           <span className={styles.helper}>{subtitle}</span>
           <p className={styles.title}>{title}</p>
         </header>
         <picture className={styles.cardPicture}>
-          <source media='(max-width: 480px)' srcSet={mobileImage} />
+          <source aria-label='media-source' media='(max-width: 480px)' srcSet={mobileImage} />
           <img alt={`${title} preview`} loading='lazy' className={styles.cardImage} src={image} />
         </picture>
       </a>
       <footer className={styles.cardFooter}>
-        <a className={styles.link} href={repository} target='_blank' rel='noopener noreferrer'>
+        <a
+          aria-label='repository-link'
+          className={styles.link}
+          href={repository}
+          target='_blank'
+          rel='noopener noreferrer'
+        >
           {t('common:cardRepositoryLink')}
-          <svg
-            className={styles.chevronRight}
-            viewBox='0 0 24 24'
-            width='24'
-            height='24'
-            stroke='currentColor'
-            strokeWidth='2'
-            fill='none'
-            strokeLinecap='round'
-            strokeLinejoin='round'
-          >
-            <polyline points='9 18 15 12 9 6' />
-          </svg>
+          <ChevronIcon />
         </a>
-        <a href={site} target='_blank' rel='noopener noreferrer' className={styles.button}>
+        <a aria-label='action-link' href={site} target='_blank' rel='noopener noreferrer' className={styles.button}>
           {category === 'web' ? t('common:cardSiteLink') : t('common:cardDownloadLink')}
         </a>
       </footer>
---
+++ components/JumboCard/index.test.js
@@ -0,0 +1,81 @@
+import React from 'react'
+import { render, screen } from '@testing-library/react'
+import JumboCard from './index'
+
+describe('Jumbo Card', () => {
+  const data = {
+    theme: 'dark',
+    color: '#f5f5f5',
+    title: 'Project Title',
+    subtitle: 'Project Subtitle',
+    image: 'https://via.placeholder.com/300x200',
+    mobileImage: 'https://via.placeholder.com/200x200',
+    repository: 'https://www.github.com/tzivigelstein/tzivigelstein',
+    site: 'https://www.tzivi.site/',
+    category: 'web'
+  }
+
+  const blankTarget = '_blank'
+  const rel = 'noopener noreferrer'
+
+  test('Should have article with data-theme', () => {
+    const component = render(<JumboCard project={data} />)
+    expect(component.container.querySelector('article[data-theme="dark"]')).toBeInTheDocument()
+  })
+
+  test('Should have anchor with href, target, and rel', () => {
+    const component = render(<JumboCard project={data} />)
+    const link = component.getByLabelText('main-link')
+
+    expect(link).toBeInTheDocument()
+    expect(link).toHaveAttribute('href', data.site)
+    expect(link).toHaveAttribute('target', blankTarget)
+    expect(link).toHaveAttribute('rel', rel)
+  })
+
+  test('Should have title', () => {
+    const component = render(<JumboCard project={data} />)
+    component.getByText(data.title)
+  })
+
+  test('Should have subtitle', () => {
+    const component = render(<JumboCard project={data} />)
+    component.getByText(data.subtitle)
+  })
+
+  test('Should have image with source and alt', () => {
+    render(<JumboCard project={data} />)
+    const image = screen.getByRole('img')
+
+    expect(image).toBeInTheDocument()
+    expect(image).toHaveAttribute('alt', `${data.title} preview`)
+    expect(image).toHaveAttribute('src', data.image)
+  })
+
+  test('Should have source with srcSet and media query', () => {
+    render(<JumboCard project={data} />)
+    const source = screen.getByLabelText('media-source')
+
+    expect(source).toBeInTheDocument()
+    expect(source).toHaveAttribute('srcset', data.mobileImage)
+  })
+
+  test('Should have repository link with href, target, and rel', () => {
+    const component = render(<JumboCard project={data} />)
+    const link = component.getByLabelText('repository-link')
+    expect(link).toBeInTheDocument()
+    expect(link).toHaveAttribute('href', data.repository)
+    expect(link).toHaveAttribute('target', blankTarget)
+    expect(link).toHaveAttribute('rel', rel)
+    expect(link).toHaveTextContent('Repository')
+  })
+
+  test('Should have action link with href, target, and rel', () => {
+    const component = render(<JumboCard project={data} />)
+    const link = component.getByLabelText('action-link')
+    expect(link).toBeInTheDocument()
+    expect(link).toHaveAttribute('href', data.site)
+    expect(link).toHaveAttribute('target', blankTarget)
+    expect(link).toHaveAttribute('rel', rel)
+  })
+})
