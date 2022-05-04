f545170a188853e16667d660d45c61a571f0af84
Tzivi Gelstein <gelsteintzivisantiago@gmail.com>
2022-05-04 15:06:52 -0300

    Setup test library

---
+++ jest.config.js
@@ -0,0 +1,19 @@
+const nextJest = require('next/jest')
+
+const createJestConfig = nextJest({
+  dir: './'
+})
+
+const customJestConfig = {
+  setupFilesAfterEnv: ['<rootDir>/jest.setup.js'],
+  moduleNameMapper: {
+    // Handle module aliases (this will be automatically configured for you soon)
+    '^@components/(.*)$': '<rootDir>/components/$1',
+
+    '^@pages/(.*)$': '<rootDir>/pages/$1'
+  },
+  testEnvironment: 'jest-environment-jsdom'
+}
+
+// createJestConfig is exported this way to ensure that next/jest can load the Next.js config which is async
+module.exports = createJestConfig(customJestConfig)
