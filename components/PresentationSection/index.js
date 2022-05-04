9d078034d911f7001d89ed447c1606c29651986e
Tzivi Gelstein <gelsteintzivisantiago@gmail.com>
2022-05-04 15:41:10 -0300

    Update presentation section component

--- components/PresentationSection/index.js
+++ components/PresentationSection/index.js
@@ -1,43 +1,44 @@
 import styles from './index.module.css'
 import useTranslation from 'next-translate/useTranslation'
 import { H2, H4 } from '@components/Heading'
-import { ParagraphText, TextEmoji } from '@components/Text'
-import { ChevronIcon } from '@components/Icons'
+import { ParagraphText } from '@components/Text'
+import ActionDeclaration from '@components/ActionDeclaration'
 
 const PresentationSection = () => {
   const { t } = useTranslation()
+
+  const ACTIONS_DECLARATIONS = [
+    {
+      id: 0,
+      title: t('common:presentationSectionListItemTitle1'),
+      text: t('common:presentationSectionListItem1'),
+      emoji: '📊'
+    },
+    {
+      id: 1,
+      title: t('common:presentationSectionListItemTitle2'),
+      text: t('common:presentationSectionListItem2'),
+      emoji: '💡'
+    },
+    {
+      id: 2,
+      title: t('common:presentationSectionListItemTitle3'),
+      text: t('common:presentationSectionListItem3'),
+      emoji: '🛠'
+    }
+  ]
 
   return (
     <section className={styles.container}>
       <H2>{t('common:presentationSectionTitle')}</H2>
       <div className={styles.information}>
-        <div>
-          <ParagraphText>{t('common:presentationSectionText')}</ParagraphText>
-          <H4>{t('common:presentationSectionListTitle')}</H4>
-          <ul className={styles.list}>
-            <li className={styles.listItem}>
-              <ChevronIcon />
-              <ParagraphText>
-                <span className={styles.bold}>{t('common:presentationSectionListItemTitle1')}</span>{' '}
-                {t('common:presentationSectionListItem1')} <TextEmoji>📊</TextEmoji>
-              </ParagraphText>
-            </li>
-            <li className={styles.listItem}>
-              <ChevronIcon />
-              <ParagraphText>
-                <span className={styles.bold}>{t('common:presentationSectionListItemTitle2')}</span>{' '}
-                {t('common:presentationSectionListItem2')} <TextEmoji>💡</TextEmoji>
-              </ParagraphText>
-            </li>
-            <li className={styles.listItem}>
-              <ChevronIcon />
-              <ParagraphText>
-                <span className={styles.bold}>{t('common:presentationSectionListItemTitle3')}</span>{' '}
-                {t('common:presentationSectionListItem3')} <TextEmoji>🛠</TextEmoji>
-              </ParagraphText>
-            </li>
-          </ul>
-        </div>
+        <ParagraphText>{t('common:presentationSectionText')}</ParagraphText>
+        <H4>{t('common:presentationSectionListTitle')}</H4>
+        <ul className={styles.list}>
+          {ACTIONS_DECLARATIONS.map(({ id, ...rest }) => (
+            <ActionDeclaration key={id} action={rest} />
+          ))}
+        </ul>
       </div>
     </section>
   )
