490a279de41914120129830b978a4dcfb7f12464
Tzivi Gelstein <gelsteintzivisantiago@gmail.com>
2022-12-26 14:27:02 -0300

    remove unused components

--- components/MDXComponents.js
+++ components/MDXComponents.js
@@ -1,10 +1,15 @@
-import { ParagraphText, TextItalic, TextLink, TextEmoji, TextListItem, List } from '@components/Text'
+import {
+  ParagraphText,
+  TextItalic,
+  TextLink,
+  TextListItem,
+  List,
+} from "@components/Text";
 
 export default {
   p: ParagraphText,
   em: TextItalic,
   TextLink,
-  TextEmoji,
   TextListItem,
-  ul: List
-}
+  ul: List,
+};
