a3b400db47975a69e9edf7dc4ca219445f31d4f4
Tzivi Gelstein <gelsteintzivisantiago@gmail.com>
2022-12-26 14:32:32 -0300

    update heading

--- components/Story/index.js
+++ components/Story/index.js
@@ -1,20 +1,20 @@
-import styles from './index.module.css'
-import { H3 } from '@components/Heading'
-import useTranslation from 'next-translate/useTranslation'
-import { MDXRemote } from 'next-mdx-remote'
-import MDXComponents from '@components/MDXComponents'
+import styles from "./index.module.css";
+import { H2 } from "@components/Heading";
+import useTranslation from "next-translate/useTranslation";
+import { MDXRemote } from "next-mdx-remote";
+import MDXComponents from "@components/MDXComponents";
 
-export default function Story ({ post }) {
-  const { t } = useTranslation()
+export default function Story({ post }) {
+  const { t } = useTranslation();
 
-  const { source } = post
+  const { source } = post;
 
   return (
     <div className={styles.storyContainer}>
-      <H3>{t('common:storyTitle')}</H3>
+      <H2>{t("common:storyTitle")}</H2>
       <div className={styles.textContainer}>
         <MDXRemote {...source} components={MDXComponents} />
       </div>
     </div>
-  )
+  );
 }
