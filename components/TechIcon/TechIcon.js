843d3f4ffa67cd310bce0dd8b96ac494250d4109
Tzivi Gelstein <gelsteintzivisantiago@gmail.com>
2022-12-26 14:32:53 -0300

    add border color

--- components/TechIcon/TechIcon.js
+++ components/TechIcon/TechIcon.js
@@ -1,21 +1,28 @@
-import styles from './techicon.module.css'
+import styles from "./techicon.module.css";
 
 const TechIcon = ({ children, name, link, styles: iconStyles }) => {
   return (
     <a
       href={link}
-      target='_blank'
-      rel='noopener noreferrer'
+      target="_blank"
+      rel="noopener noreferrer"
       className={styles.iconLink}
     >
-      <div style={iconStyles} className={styles.skill}>
+      <div
+        style={
+          iconStyles.backgroundColor === "#fff"
+            ? {
+                ...iconStyles,
+                borderColor: "#d2d2d7",
+              }
+            : iconStyles
+        }
+        className={styles.skill}
+      >
         {children}
       </div>
-      <div translate='no' className={styles.skillTooltip}>
-        <span>{name}</span>
-      </div>
     </a>
-  )
-}
+  );
+};
 
-export default TechIcon
+export default TechIcon;
