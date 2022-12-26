9f39583ec41195bd671d758d0a9b5b4c4eaa6043
Tzivi Gelstein <gelsteintzivisantiago@gmail.com>
2022-12-26 14:31:36 -0300

    refactor presentation section

--- components/PresentationSection/index.js
+++ components/PresentationSection/index.js
@@ -1,47 +1,43 @@
-import styles from './index.module.css'
-import useTranslation from 'next-translate/useTranslation'
-import { H2, H4 } from '@components/Heading'
-import { ParagraphText } from '@components/Text'
-import ActionDeclaration from '@components/ActionDeclaration'
+import styles from "./index.module.css";
+import useTranslation from "next-translate/useTranslation";
+import { H2, H4 } from "@components/Heading";
+import ActionDeclaration from "@components/ActionDeclaration";
+import { BuildIcon, PuzzlePieceIcon, SpeedMeterIcon } from "@components/Icons";
 
 const PresentationSection = () => {
-  const { t } = useTranslation()
+  const { t } = useTranslation();
 
   const ACTIONS_DECLARATIONS = [
     {
       id: 0,
-      title: t('common:presentationSectionListItemTitle1'),
-      text: t('common:presentationSectionListItem1'),
-      emoji: '📊'
+      title: t("common:presentationSectionListItemTitle1"),
+      text: t("common:presentationSectionListItem1"),
+      icon: <SpeedMeterIcon fill="#FF6600"/>,
     },
     {
       id: 1,
-      title: t('common:presentationSectionListItemTitle2'),
-      text: t('common:presentationSectionListItem2'),
-      emoji: '💡'
+      title: t("common:presentationSectionListItemTitle2"),
+      text: t("common:presentationSectionListItem2"),
+      icon: <PuzzlePieceIcon width={190} height={190} fill="#6699CC" />,
     },
     {
       id: 2,
-      title: t('common:presentationSectionListItemTitle3'),
-      text: t('common:presentationSectionListItem3'),
-      emoji: '🛠'
-    }
-  ]
+      title: t("common:presentationSectionListItemTitle3"),
+      text: t("common:presentationSectionListItem3"),
+      icon: <BuildIcon width={190} height={190} fill="#99CC00" />,
+    },
+  ];
 
   return (
     <section className={styles.container}>
-      <H2>{t('common:presentationSectionTitle')}</H2>
-      <div className={styles.information}>
-        <ParagraphText>{t('common:presentationSectionText')}</ParagraphText>
-        <H4>{t('common:presentationSectionListTitle')}</H4>
-        <ul className={styles.list}>
-          {ACTIONS_DECLARATIONS.map(({ id, ...rest }) => (
-            <ActionDeclaration key={id} action={rest} />
-          ))}
-        </ul>
-      </div>
+      <H2>{t("common:presentationSectionTitle")}</H2>
+      <ul className={styles.list}>
+        {ACTIONS_DECLARATIONS.map(({ id, ...rest }) => (
+          <ActionDeclaration key={id} action={rest} />
+        ))}
+      </ul>
     </section>
-  )
-}
+  );
+};
 
-export default PresentationSection
+export default PresentationSection;
