b2480ca9afe84150b077c0fe3fa225fecb11fa1e
Tzivi Gelstein <gelsteintzivisantiago@gmail.com>
2022-12-26 14:33:00 -0300

    update text

--- components/Text/index.js
+++ components/Text/index.js
@@ -1,32 +1,45 @@
-import styles from './index.module.css'
+import styles from "./index.module.css";
 
-export const ParagraphText = ({ children }) => <p className={styles.paragraphText}>{children}</p>
+export const ParagraphText = ({ children, style }) => (
+  <p style={style} className={styles.paragraphText}>
+    {children}
+  </p>
+);
 export const ParagraphTextBig = ({ children }) => (
-  <p className={`${styles.paragraphTextBig} ${styles.paragraphText}`}>{children}</p>
-)
+  <p className={`${styles.paragraphTextBig} ${styles.paragraphText}`}>
+    {children}
+  </p>
+);
 
-export const TextBold = ({ children }) => <span className={styles.textBold}>{children}</span>
+export const TextBold = ({ children }) => (
+  <span className={styles.textBold}>{children}</span>
+);
 
 export const TextLink = ({ children, href }) => (
-  <a rel='noopener noreferrer' target='_blank' className={styles.textLink} href={href}>
+  <a
+    rel="noopener noreferrer"
+    target="_blank"
+    className={styles.textLink}
+    href={href}
+  >
     {children}
   </a>
-)
+);
 
 export const TextItalic = ({ children, ...props }) => (
   <span className={styles.textItalic} {...props}>
     {children}
   </span>
-)
-
-export const TextEmoji = ({ children }) => <span className={styles.textEmoji}>{children}</span>
+);
 
 export const TextListItem = ({ children }) => {
   return (
     <li>
       <span className={styles.textListItem}>{children}</span>
     </li>
-  )
-}
+  );
+};
 
-export const List = ({ children }) => <ul className={styles.list}>{children}</ul>
+export const List = ({ children }) => (
+  <ul className={styles.list}>{children}</ul>
+);
