cf9ade9caea2b536c00084c8a0faba19433664e4
Tzivi Gelstein <gelsteintzivisantiago@gmail.com>
2022-12-26 14:29:50 -0300

    refactor header link

--- components/HeaderLink/index.js
+++ components/HeaderLink/index.js
@@ -1,11 +1,18 @@
-import styles from './index.module.css'
+import { ChevronIcon } from "@components/Icons";
+import styles from "./index.module.css";
 
 const HeaderLink = ({ text, link }) => {
   return (
-    <a className={styles.headerLink} href={link} target='_blank' rel='noopener noreferrer'>
+    <a
+      className={styles.headerLink}
+      href={link}
+      target="_blank"
+      rel="noopener noreferrer"
+    >
       {text}
+      <ChevronIcon />
     </a>
-  )
-}
+  );
+};
 
-export default HeaderLink
+export default HeaderLink;
