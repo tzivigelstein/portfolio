31b9754db0695e0dd9eb4b1ac27b35c018395700
Tzivi Gelstein <gelsteintzivisantiago@gmail.com>
2022-12-26 14:33:24 -0300

    refactor code style

--- pages/index.js
+++ pages/index.js
@@ -1,24 +1,24 @@
-import Container from '@components/Container'
-import Header from '@components/Header'
-import Sections from '@components/SectionsContainer'
-import Footer from '@components/Footer'
-import ControlBar from '@components/ControlBar'
-import { getFileBySlug } from 'lib/mdx'
+import Container from "@components/Container";
+import Header from "@components/Header";
+import Sections from "@components/SectionsContainer";
+import Footer from "@components/Footer";
+import { getFileBySlug } from "lib/mdx";
 
-export default function Home ({ post }) {
+export default function Home({ post }) {
   return (
     <Container>
-      <ControlBar />
       <Header />
       <Sections post={post} />
       <Footer />
     </Container>
-  )
+  );
 }
 
-export async function getStaticProps (context) {
-  const { locale } = context
-  const post = await getFileBySlug({ slug: `my-story-${locale === 'es' ? 'es' : 'en'}` })
+export async function getStaticProps(context) {
+  const { locale } = context;
+  const post = await getFileBySlug({
+    slug: `my-story-${locale === "es" ? "es" : "en"}`,
+  });
 
-  return { props: { post } }
+  return { props: { post } };
 }
