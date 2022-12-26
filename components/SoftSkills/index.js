e2e541310611b9da5a144cccc206da1f945a0727
Tzivi Gelstein <gelsteintzivisantiago@gmail.com>
2022-12-26 14:32:20 -0300

    refactor soft skills

--- components/SoftSkills/index.js
+++ components/SoftSkills/index.js
@@ -1,23 +1,20 @@
-import { TextEmoji } from '@components/Text'
-import useTranslation from 'next-translate/useTranslation'
-import styles from './index.module.css'
+import { H4 } from "@components/Heading";
+import useTranslation from "next-translate/useTranslation";
+import styles from "./index.module.css";
 
-export default function SoftSkills () {
-  const { t } = useTranslation()
+export default function SoftSkills() {
+  const { t } = useTranslation();
 
   return (
     <div>
-      <h3 className={styles.subtitle}>
-        {t('common:softSkillsTitle')} <TextEmoji>🦸‍♂️</TextEmoji>
-      </h3>
+      <H4>{t("common:softSkillsTitle")}</H4>
       <div className={styles.goalsContainer}>
-        <span className={styles.goal}>
-          <TextEmoji>👉</TextEmoji> {t('common:softSkillsFirstTip')}
-        </span>
-        <span className={styles.goal}>
-          <TextEmoji>👉</TextEmoji> {t('common:softSkillsSecondTip')}
-        </span>
+        <span className={styles.goal}>{t("common:softSkillsFirstTip")}</span>
+        <span className={styles.goal}>{t("common:softSkillsSecondTip")}</span>
+        <span className={styles.goal}>{t("common:softSkillsThirdTip")}</span>
+        <span className={styles.goal}>{t("common:softSkillsFourthTip")}</span>
+        <span className={styles.goal}>{t("common:softSkillsFifthTip")}</span>
       </div>
     </div>
-  )
+  );
 }
