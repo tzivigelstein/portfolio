d9c2f9b3114299cd68b8190dd17f7e29cc9f6c13
Tzivi Gelstein <gelsteintzivisantiago@gmail.com>
2022-12-26 14:33:34 -0300

    update data

--- data/projects.js
+++ data/projects.js
@@ -1,187 +1,123 @@
 const projects = [
   {
-    id: 'Zsu',
-    category: 'web',
-    title: 'Wordle',
-    subtitle: 'Entertainment App',
-    image: '/images/Wordle.jpg',
-    mobileImage: '/images/Wordle-mobile.jpg',
-    repository: 'https://github.com/tzivigelstein/wordle',
-    site: 'https://thisisnotwordle.vercel.app/',
-    stack: [
-      { id: 'XH', name: 'React', color: '#5ed3f3' },
-      { id: 'Ms', name: 'Node', color: '#7fc728' },
-      { id: '4a', name: 'CSS', color: '#006bb4' }
-    ],
-    color: '#6aaa64',
-    theme: 'dark'
+    id: "Zsu",
+    category: "web",
+    title: "Wordle",
+    subtitle: "Entertainment App",
+    image: "/images/Wordle.jpg",
+    mobileImage: "/images/Wordle-mobile.jpg",
+    repository: "https://github.com/tzivigelstein/wordle",
+    site: "https://thisisnotwordle.vercel.app/",
+    color: "#6aaa64",
+    theme: "dark",
   },
   {
-    id: 'Oai',
-    category: 'web',
-    title: 'Minity',
-    subtitle: 'Productivity App',
-    image: '/images/Minity.webp',
-    repository: 'https://github.com/tzivigelstein/minity',
-    site: 'https://minity.vercel.app/',
-    stack: [
-      { id: 'XH', name: 'React', color: '#5ed3f3' },
-      { id: 'Ms', name: 'NextJS', color: '#7fc728' },
-      { id: '4a', name: 'CSS', color: '#006bb4' }
-    ],
-    theme: 'dark',
-    color: '#3f92d4'
+    id: "Oai",
+    category: "web",
+    title: "Minity",
+    subtitle: "Productivity App",
+    image: "/images/Minity.webp",
+    repository: "https://github.com/tzivigelstein/minity",
+    site: "https://minity.vercel.app/",
+    theme: "dark",
+    color: "#3f92d4",
   },
   {
-    id: 'O1i',
-    category: 'web',
-    title: 'Adivina palabra',
-    subtitle: 'Entertainment App',
-    image: '/images/AdivinaPalabra.png',
-    repository: 'https://github.com/tzivigelstein/guessword',
-    site: 'https://adivina-palabra.vercel.app/',
-    stack: [
-      { id: 'XH', name: 'React', color: '#5ed3f3' },
-      { id: 'Ms', name: 'Node', color: '#7fc728' },
-      { id: '4a', name: 'CSS', color: '#006bb4' }
-    ],
-    theme: 'dark',
-    color: '#8ca0d7'
+    id: "O1i",
+    category: "web",
+    title: "Adivina palabra",
+    subtitle: "Entertainment App",
+    image: "/images/AdivinaPalabra.png",
+    repository: "https://github.com/tzivigelstein/guessword",
+    site: "https://adivina-palabra.vercel.app/",
+    theme: "dark",
+    color: "#8ca0d7",
   },
   {
-    id: 'SGE',
-    category: 'web',
-    title: 'Tidify Web',
-    subtitle: 'Food App',
-    image: '/images/TidifyFlyerWeb.webp',
-    repository: 'https://github.com/tzivigelstein/Tidify-Host',
-    site: 'https://tidify-app.vercel.app/',
-    stack: [
-      { id: 'im', name: 'React', color: '#5ed3f3' },
-      { id: 'ce', name: 'CSS', color: '#006bb4' },
-      { id: 'J8', name: 'Firebase', color: '#f7c62d' }
-    ],
-    color: '#ccff97'
+    id: "SGE",
+    category: "web",
+    title: "Tidify Web",
+    subtitle: "Food App",
+    image: "/images/TidifyFlyerWeb.webp",
+    repository: "https://github.com/tzivigelstein/Tidify-Host",
+    site: "https://tidify-app.vercel.app/",
+    color: "#ccff97",
   },
   {
-    id: '2Jp',
-    category: 'web',
-    title: 'Product Hunt',
-    subtitle: 'Social App',
-    image: '/images/PHuntFlyer.webp',
-    repository: 'https://github.com/tzivigelstein/Product-Hunt-Clone',
-    site: 'https://producthunt-cl.vercel.app/',
-    stack: [
-      { id: 'sI', name: 'React', color: '#5ed3f3' },
-      { id: 'Nq', name: 'Next', color: '#222' },
-      { id: 'vZ', name: 'CSS', color: '#006bb4' },
-      { id: 'n3', name: 'Firebase', color: '#f7c62d' }
-    ],
-    color: 'linear-gradient(90deg, #feaf04, #fa9814)'
+    id: "2Jp",
+    category: "web",
+    title: "Product Hunt",
+    subtitle: "Social App",
+    image: "/images/PHuntFlyer.webp",
+    repository: "https://github.com/tzivigelstein/Product-Hunt-Clone",
+    site: "https://producthunt-cl.vercel.app/",
+    color: "linear-gradient(90deg, #feaf04, #fa9814)",
   },
   {
-    id: 'gCh',
-    category: 'web',
-    title: 'Twitter',
-    subtitle: 'Social App',
-    image: '/images/TwitterCloneFlyer.webp',
-    repository: 'https://github.com/tzivigelstein/Twitter-Clone',
-    site: 'https://twitter-cl.vercel.app/login',
-    stack: [
-      { id: 'wf', name: 'React', color: '#5ed3f3' },
-      { id: 'Lx', name: 'Next', color: '#222' },
-      { id: 'gG', name: 'CSS', color: '#006bb4' },
-      { id: 'Dv', name: 'Firebase', color: '#f7c62d' }
-    ],
-    color: '#fff'
+    id: "gCh",
+    category: "web",
+    title: "Twitter",
+    subtitle: "Social App",
+    image: "/images/TwitterCloneFlyer.webp",
+    repository: "https://github.com/tzivigelstein/Twitter-Clone",
+    site: "https://twitter-cl.vercel.app/login",
+    color: "#fff",
   },
   {
-    id: '0T9',
-    category: 'web',
-    title: 'Lyric it',
-    subtitle: 'Entertainment App',
-    image: '/images/LyricItFlyer.webp',
-    repository: 'https://github.com/tzivigelstein/Lyric-it',
-    site: 'https://lyric-it.netlify.app/',
-    stack: [
-      { id: 'wf', name: 'React', color: '#5ed3f3' },
-      { id: 'gG', name: 'Bootstrap', color: '#7952b3' }
-    ],
-    color: '#ef476f',
-    theme: 'dark'
+    id: "0T9",
+    category: "web",
+    title: "Lyric it",
+    subtitle: "Entertainment App",
+    image: "/images/LyricItFlyer.webp",
+    repository: "https://github.com/tzivigelstein/Lyric-it",
+    site: "https://lyric-it.netlify.app/",
+    color: "#ef476f",
+    theme: "dark",
   },
   {
-    id: '2NB',
-    category: 'web',
-    title: 'Binast',
-    subtitle: 'Productivity App',
-    image: '/images/BinastWeb.webp',
-    repository: 'https://github.com/tzivigelstein/Binast',
-    site: 'https://binast.vercel.app/',
-    stack: [
-      { id: 'gG', name: 'CSS', color: '#006bb4' },
-      { id: 'Lx', name: 'Next', color: '#222' }
-    ],
-    color: 'linear-gradient(90deg, #977EF1, #623CEA)',
-    theme: 'dark'
+    id: "2NB",
+    category: "web",
+    title: "Binast",
+    subtitle: "Productivity App",
+    image: "/images/BinastWeb.webp",
+    repository: "https://github.com/tzivigelstein/Binast",
+    site: "https://binast.vercel.app/",
+    color: "linear-gradient(90deg, #977EF1, #623CEA)",
+    theme: "dark",
   },
   {
-    id: 'jyO',
-    category: 'app',
-    title: 'Minity App',
-    subtitle: 'Productivity App',
-    image: '/images/MinityApp.webp',
-    repository: 'https://github.com/tzivigelstein/Minity-app',
-    site: 'https://tzivi.site/android/minity-app.apk/',
-    stack: [
-      { id: 'eI', name: 'React Native', color: '#03a3ce' },
-      { id: 'K3', name: 'Node', color: '#7fc728' },
-      { id: 'GQ', name: 'GraphQL', color: '#de33a6' }
-    ],
-    color: 'linear-gradient(90deg, #005893, #0081bf)',
-    theme: 'dark'
+    id: "jyO",
+    category: "app",
+    title: "Minity App",
+    subtitle: "Productivity App",
+    image: "/images/MinityApp.webp",
+    repository: "https://github.com/tzivigelstein/Minity-app",
+    site: "https://tzivi.site/android/minity-app.apk/",
+    color: "linear-gradient(90deg, #005893, #0081bf)",
+    theme: "dark",
   },
   {
-    id: 'lt4',
-    category: 'app',
-    title: 'Crypto',
-    subtitle: 'Productivity App',
-    image: '/images/CryptoFlyerApp.webp',
-    repository: 'https://github.com/tzivigelstein/Crypto-App',
-    site: 'https://tzivi.site/android/crypto-app.apk/',
-    apk: 'Crypto',
-    stack: [
-      {
-        id: 'JV',
-        name: 'React Native',
-        color: '#03a3ce'
-      }
-    ],
-    color: '#8efdf3'
+    id: "lt4",
+    category: "app",
+    title: "Crypto",
+    subtitle: "Productivity App",
+    image: "/images/CryptoFlyerApp.webp",
+    repository: "https://github.com/tzivigelstein/Crypto-App",
+    site: "https://tzivi.site/android/crypto-app.apk/",
+    apk: "Crypto",
+    color: "#8efdf3",
   },
   {
-    id: 'lKc',
-    category: 'app',
-    title: 'Tidify App',
-    subtitle: 'Food App',
-    image: '/images/TidifyFlyerApp.webp',
-    repository: 'https://github.com/tzivigelstein/Tidify-Client',
-    site: 'https://tzivi.site/android/tidify-app.apk/',
-    stack: [
-      {
-        id: 'JV',
-        name: 'React Native',
-        color: '#03a3ce'
-      },
-      {
-        id: 'Go',
-        name: 'Firebase',
-        color: '#f7c62d'
-      }
-    ],
-    color: '#f2917e',
-    theme: 'dark'
-  }
-]
+    id: "lKc",
+    category: "app",
+    title: "Tidify App",
+    subtitle: "Food App",
+    image: "/images/TidifyFlyerApp.webp",
+    repository: "https://github.com/tzivigelstein/Tidify-Client",
+    site: "https://tzivi.site/android/tidify-app.apk/",
+    color: "#f2917e",
+    theme: "dark",
+  },
+];
 
-export default projects
+export default projects;
--- data/technologies.js
+++ data/technologies.js
@@ -1,71 +1,56 @@
 import {
-  CSSIcon,
+  DockerIcon,
   GitIcon,
-  HTMLIcon,
-  JavaScriptIcon,
+  Linux,
   MongoIcon,
-  NextJSIcon,
-  NodeJSIcon,
-  ReactIcon
-} from '@components/Icons'
+  PostgreSQL,
+  ReactIcon,
+  TypeScript,
+} from "@components/Icons";
 
 const technologies = [
   {
-    id: 'la',
-    link: 'https://reactjs.org/',
-    name: 'ReactJS',
-    styles: { backgroundColor: '#61DAFB' },
-    Icon: () => <ReactIcon fill='#eee' width={27} height={27} />
+    id: "la",
+    link: "https://reactjs.org/",
+    styles: { backgroundColor: "#61DAFB" },
+    Icon: () => <ReactIcon fill="#eee" width={27} height={27} />,
   },
   {
-    id: 'rh',
-    link: 'https://www.mongodb.com/',
-    name: 'MongoDB',
-    styles: { backgroundColor: '#13AA52' },
-    Icon: () => <MongoIcon fill='#eee' width={27} height={27} />
+    id: "rh",
+    link: "https://www.mongodb.com/",
+    styles: { backgroundColor: "#13AA52" },
+    Icon: () => <MongoIcon fill="#eee" width={27} height={27} />,
   },
   {
-    id: 'GY',
-    link: 'https://www.javascript.com/',
-    name: 'JavaScript',
-    styles: { backgroundColor: '#FCDC00' },
-    Icon: () => <JavaScriptIcon fill='#222' width={27} height={27} />
+    id: "GY",
+    link: "https://www.docker.com/",
+    styles: { backgroundColor: "#fff" },
+    Icon: () => <DockerIcon width={27} height={27} />,
   },
   {
-    id: 'XI',
-    link: 'https://nodejs.org/',
-    name: 'NodeJS',
-    styles: { backgroundColor: '#13AA52' },
-    Icon: () => <NodeJSIcon fill='#eee' width={27} height={27} />
+    id: "XI",
+    link: "https://www.postgresql.org/",
+    styles: { backgroundColor: "#fff" },
+    Icon: () => <PostgreSQL width={27} height={27} />,
   },
   {
-    id: 'VS',
-    link: 'https://developer.mozilla.org/en-US/docs/Web/CSS',
-    name: 'CSS',
-    styles: { backgroundColor: '#249CDA' },
-    Icon: () => <CSSIcon fill='#eee' width={27} height={27} />
+    id: "Zl",
+    link: "https://git-scm.com/",
+    styles: { backgroundColor: "#F14E32" },
+    Icon: () => <GitIcon fill="#eee" width={27} height={27} />,
   },
   {
-    id: 'kt',
-    link: 'https://developer.mozilla.org/en-US/docs/Glossary/HTML',
-    name: 'HTML',
-    styles: { backgroundColor: '#F64A1D' },
-    Icon: () => <HTMLIcon fill='#eee' width={27} height={27} />
+    id: "0V",
+    link: "https://www.linux.org/",
+    styles: { backgroundColor: "#fff" },
+    Icon: () => <Linux width={27} />,
   },
   {
-    id: 'Zl',
-    link: 'https://git-scm.com/',
-    name: 'Git',
-    styles: { backgroundColor: '#F14E32' },
-    Icon: () => <GitIcon fill='#eee' width={27} height={27} />
+    id: "XZ",
+    link: "https://www.typescriptlang.org/",
+    styles: { backgroundColor: "#007ACC" },
+    Icon: () => <TypeScript width={27} height={27} />,
   },
-  {
-    id: '0V',
-    link: 'https://www.nextjs.org/',
-    name: 'NextJS',
-    styles: { backgroundColor: '#fff' },
-    Icon: () => <NextJSIcon fill='#000' width={27} height={27} />
-  }
-]
+];
 
-export default technologies
+export default technologies;
