a3eae26672e64b42a3ce2bbfefcfb9dd811c6675
Tzivi Gelstein <gelsteintzivisantiago@gmail.com>
2023-03-18 13:10:43 -0300

    refactor(_app): update browser accent color

--- pages/_app.js
+++ pages/_app.js
@@ -8,7 +8,7 @@ const MyApp = ({ Component, pageProps }) => (
       <meta charSet='utf-8' />
       <link rel='icon' type='image/png' href='/favicon.png' />
       <meta name='viewport' content='width=device-width, initial-scale=1' />
-      <meta name='theme-color' content='#6ec2f7' />
+      <meta name='theme-color' content='#fff' />
 
       <title>Tzivi Gelstein | React developer</title>
       <meta name='title' content='Tzivi | Portfolio' />
