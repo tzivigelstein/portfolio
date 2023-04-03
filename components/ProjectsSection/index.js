4b2b5cbc8b20358e15de61d30c019c27e76eb6d1
Tzivi Gelstein <gelsteintzivisantiago@gmail.com>
2023-04-03 19:10:48 -0300

    update projects section

--- components/ProjectsSection/index.js
+++ components/ProjectsSection/index.js
@@ -1,37 +1,49 @@
-import styles from './index.module.css'
-import projects from '@data/projects'
-import Card from '@components/Card'
-import JumboCard from '@components/JumboCard'
-import useTranslation from 'next-translate/useTranslation'
+import styles from "./index.module.css";
+import projects from "@data/projects";
+import Card from "@components/Card";
+import JumboCard from "@components/JumboCard";
+import useTranslation from "next-translate/useTranslation";
 
 const ProjectsSection = () => {
-  const { t } = useTranslation()
+  const { t } = useTranslation();
 
-  const webs = projects.filter(({ category }) => category === 'web')
-  const jumbo = webs.shift()
-  const apps = projects.filter(({ category }) => category === 'app')
+  const webs = projects.filter(
+    ({ category, active }) => category === "web" && active
+  );
+  const jumbo = webs.shift();
+  const apps = projects.filter(
+    ({ category, active }) => category === "app" && active
+  );
 
   return (
     <section className={styles.container}>
       <section className={styles.secondarySection}>
-        <h2 className={styles.sectionTitle}>{t('common:projectsSectionWebTitle')}</h2>
-        <JumboCard jumbo key={jumbo.id} project={jumbo} />
+        <h2 className={styles.sectionTitle}>
+          {t("common:projectsSectionWebTitle")}
+        </h2>
+        <JumboCard project={jumbo} />
         <ul className={styles.projectList}>
-          {webs.map(project => (
+          {webs.map((project) => (
             <Card key={project.id} project={project} />
           ))}
         </ul>
       </section>
       <section className={styles.secondarySection}>
-        <h2 className={styles.sectionTitle}>{t('common:projectsSectionAppTitle')}</h2>
-        <ul className={styles.projectList}>
-          {apps.map(project => (
-            <Card key={project.id} project={project} />
-          ))}
-        </ul>
+        <h2 className={styles.sectionTitle}>
+          {t("common:projectsSectionAppTitle")}
+        </h2>
+        {apps.length === 1 ? (
+          <JumboCard project={apps[0]} />
+        ) : (
+          <ul className={styles.projectList}>
+            {apps.map((project) => (
+              <Card key={project.id} project={project} />
+            ))}
+          </ul>
+        )}
       </section>
     </section>
-  )
-}
+  );
+};
 
-export default ProjectsSection
+export default ProjectsSection;
