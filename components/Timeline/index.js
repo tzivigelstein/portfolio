b51f36dd738f8055c02f76c19c2e23c2f66fe627
Tzivi Gelstein <gelsteintzivisantiago@gmail.com>
2023-04-04 14:33:12 -0300

    update timeline styling

--- components/Timeline/index.js
+++ components/Timeline/index.js
@@ -3,28 +3,37 @@ import {
   VerticalTimelineElement,
 } from "react-vertical-timeline-component";
 import "react-vertical-timeline-component/style.min.css";
+import styles from "./index.module.css";
 
 export default function Timeline({ timeline }) {
   return (
-    <VerticalTimeline animate={false}>
+    <VerticalTimeline animate={false} lineColor="var(--timeline-secondary)">
       {timeline.map(
         ({ id, title, description, date, icon, timeSpan, link }) => {
           return (
             <VerticalTimelineElement
               key={id}
               className="vertical-timeline-element--work"
-              contentStyle={{ background: "rgb(33, 150, 243)", color: "#fff" }}
+              contentStyle={{
+                background: "var(--timeline-main)",
+                color: "#fff",
+                borderRadius: "1rem",
+              }}
               contentArrowStyle={{
-                borderRight: "7px solid  rgb(33, 150, 243)",
+                borderRight: "7px solid  var(--timeline-main)",
               }}
               date={date ? (timeSpan ? `${date} (${timeSpan})` : date) : ""}
-              iconStyle={{ background: "rgb(33, 150, 243)", color: "#fff" }}
+              iconStyle={{
+                background: "var(--timeline-accent)",
+                color: "#fff",
+              }}
+              dateClassName={styles.date}
               icon={icon ?? null}
             >
               <h3 className="vertical-timeline-element-title">{title}</h3>
               {description}
               {link && (
-                <a target="_blank" rel="noopener noreferrer" href={link}>
+                <a target="_blank" rel="noopener noreferrer" className={styles.link} href={link}>
                   {link}
                 </a>
               )}
