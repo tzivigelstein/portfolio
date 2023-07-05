2bf46fe03c245d95cb664781ed30c7ee8a1afddc
Tzivi Gelstein <gelsteintzivisantiago@gmail.com>
2023-07-05 19:34:50 -0300

    add id based styles

--- components/ActionDeclaration/index.js
+++ components/ActionDeclaration/index.js
@@ -2,14 +2,18 @@ import { H3, H4 } from "@components/Heading";
 import styles from "./index.module.css";
 
 export default function ActionDeclaration({ action }) {
-  const { title, text, icon } = action;
+  const { title, text, icon, slug } = action;
   const sharedStyles = { textAlign: "center", margin: 0, color: "inherit" };
 
   return (
     <li className={styles.listItem}>
       <H4 style={{ ...sharedStyles, textTransform: "uppercase" }}>{title}</H4>
       <H3 style={{ ...sharedStyles, marginBottom: 32 }}>{text}</H3>
-      {icon && <div className={styles.icon}>{icon}</div>}
+      {icon && (
+        <div className={styles.icon} id={slug}>
+          {icon}
+        </div>
+      )}
     </li>
   );
 }
