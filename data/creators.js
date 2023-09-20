4b122a7c529871ca79841fa6f5b559567143584f
Tzivi Gelstein <gelsteintzivisantiago@gmail.com>
2023-09-20 16:03:31 -0300

    format and change creator image

--- data/creators.js
+++ data/creators.js
@@ -1,56 +1,59 @@
 export default [
   {
     id: 0,
-    name: 'Miguel Ángel Durán',
-    image: 'https://avatars.githubusercontent.com/u/1561955?v=4',
-    website: 'https://www.midu.dev'
+    name: "Miguel Ángel Durán",
+    image: "https://avatars.githubusercontent.com/u/1561955?v=4",
+    website: "https://www.midu.dev",
   },
   {
     id: 1,
-    name: 'Oscar Barajas',
-    image: 'https://arepa.s3.amazonaws.com/oscar-barajas-tavares.png',
-    website: 'https://gndx.dev'
+    name: "Oscar Barajas",
+    image: "https://gndx.dev/_astro/gndx-512_2vy8H4.webp",
+    website: "https://gndx.dev",
   },
   {
     id: 2,
-    name: 'Daniel Shiffman',
-    image: 'https://tisch.nyu.edu/content/dam/tisch/itp/GeneralPics/DanielShiffman1.png',
-    website: 'https://shiffman.net'
+    name: "Daniel Shiffman",
+    image:
+      "https://tisch.nyu.edu/content/dam/tisch/itp/GeneralPics/DanielShiffman1.png",
+    website: "https://shiffman.net",
   },
   {
     id: 3,
-    name: 'Freddy Vega',
-    image: 'https://freddyvega.com/content/images/2020/08/freddy-vega-2.jpg',
-    website: 'https://freddyvega.com'
+    name: "Freddy Vega",
+    image: "https://freddyvega.com/content/images/2020/08/freddy-vega-2.jpg",
+    website: "https://freddyvega.com",
   },
   {
     id: 4,
-    name: 'Antonio Sarosi',
-    image: 'https://avatars.githubusercontent.com/u/54243067?v=4',
-    website: 'https://github.com/antoniosarosi'
+    name: "Antonio Sarosi",
+    image: "https://avatars.githubusercontent.com/u/54243067?v=4",
+    website: "https://github.com/antoniosarosi",
   },
   {
     id: 5,
-    name: 'Fireship',
-    image: 'https://yt3.ggpht.com/ytc/AKedOLTcIl6kKt3lEPJEySUf_hpHiKDKiFeo9eWPReLysQ=s900-c-k-c0x00ffffff-no-rj',
-    website: 'https://fireship.io/'
+    name: "Fireship",
+    image:
+      "https://yt3.ggpht.com/ytc/AKedOLTcIl6kKt3lEPJEySUf_hpHiKDKiFeo9eWPReLysQ=s900-c-k-c0x00ffffff-no-rj",
+    website: "https://fireship.io/",
   },
   {
     id: 6,
-    name: 'Los apuntes de Majo',
-    image: 'https://losapuntesdemajo.vercel.app/images/lam_logo.png',
-    website: 'https://losapuntesdemajo.vercel.app/'
+    name: "Los apuntes de Majo",
+    image: "https://losapuntesdemajo.vercel.app/images/lam_logo.png",
+    website: "https://losapuntesdemajo.vercel.app/",
   },
   {
     id: 7,
-    name: 'Juan Jose Ortiz',
-    image: 'https://img-c.udemycdn.com/user/200_H/13799158_f1d1_3.jpg',
-    website: 'https://github.com/juanortiz10'
+    name: "Juan Jose Ortiz",
+    image: "https://img-c.udemycdn.com/user/200_H/13799158_f1d1_3.jpg",
+    website: "https://github.com/juanortiz10",
   },
   {
     id: 8,
-    name: 'Kevin Powell',
-    image: 'https://d33wubrfki0l68.cloudfront.net/554ca802d12f9823e5449ae62bc6bc2775c5c67a/a52fd/img/f21b00ed-600.avif',
-    website: 'https://www.kevinpowell.co/'
-  }
-]
+    name: "Kevin Powell",
+    image:
+      "https://d33wubrfki0l68.cloudfront.net/554ca802d12f9823e5449ae62bc6bc2775c5c67a/a52fd/img/f21b00ed-600.avif",
+    website: "https://www.kevinpowell.co/",
+  },
+];
