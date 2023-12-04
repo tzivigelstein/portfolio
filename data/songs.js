f80ec5fc1b916c569cd20ab11d7211302c2dd8dd
Tzivi Gelstein <gelsteintzivisantiago@gmail.com>
2023-12-03 21:03:03 -0300

    add songs

---
+++ data/songs.js
@@ -0,0 +1,37 @@
+export default [
+  {
+    id: 1,
+    title: "A Silent Night Meditation",
+    url: "https://files.freemusicarchive.org/storage-freemusicarchive-org/tracks/b49oLLzsW9Wt8pGaNMugaro0OuJoSJzKUGqVvOUd.mp3",
+    previewImage:
+      "https://freemusicarchive.org/image/?file=album_image%2FTscwo5K56ozRXVYUbbFp2EvqchYvmEHXbOCLMrCA.jpg&width=290&height=290&type=album",
+  },
+  {
+    id: 2,
+    title: "First Snow",
+    url: "https://files.freemusicarchive.org/storage-freemusicarchive-org/tracks/aNMGi5zWqUxP0qP9De7WJSEV43p0mkZiFrJGuljD.mp3",
+    previewImage:
+      "https://freemusicarchive.org/image/?file=album_image%2F8fbwBtv07nezpm8y3hvW1RhligQSCsNyzKZePS7l.png&width=290&height=290&type=album",
+  },
+  {
+    id: 3,
+    title: "Oh Holy Night (Lo-Fi)",
+    url: "https://files.freemusicarchive.org/storage-freemusicarchive-org/tracks/3lD7yhbHYuYMoraKq7etTlkeCNJzl17OT1nVbLAG.mp3",
+    previewImage:
+      "https://freemusicarchive.org/image/?file=album_image%2FdqeE5Wy0pQhhjlxpNyyFRCOtQli50jt3pb1ET6kX.jpg&width=290&height=290&type=album",
+  },
+  {
+    id: 4,
+    title: "Happy Christmas",
+    url: "https://files.freemusicarchive.org/storage-freemusicarchive-org/tracks/qbVPEyzxeITTWQ7Ikd9V1kkGMGBBAcXQBJUdgAT7.mp3",
+    previewImage:
+      "https://freemusicarchive.org/image/?file=track_image%2FjKJYYNHtlRULzUsUKwkdyWJfrWU9jIPfBQNpjlGB.jpg&width=290&height=290&type=track",
+  },
+  {
+    id: 5,
+    title: "Christmas Jazz",
+    url: "https://files.freemusicarchive.org/storage-freemusicarchive-org/tracks/Tl3HlWGh9LhNNw8ZTfF2jiwOekNYjpFupaR3pth7.mp3",
+    previewImage:
+      "https://freemusicarchive.org/image/?file=track_image%2FrO7ZFYy2fWC9BXWXWHu6tHRN7XnI4ESjdehSYJy3.png&width=290&height=290&type=track",
+  },
+];
