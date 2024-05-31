3f81b6a5cf137059e0260b142fb8319feb1d8762
Tzivi Gelstein <gelsteintzivisantiago@gmail.com>
2024-05-31 16:47:47 -0300

    extract repeated code

--- components/ProjectsSection/index.js
+++ components/ProjectsSection/index.js
@@ -1,27 +1,23 @@
-import styles from "./index.module.css";
-import projects from "@data/projects";
-import Card from "@components/Card";
-import JumboCard from "@components/JumboCard";
-import useTranslation from "next-translate/useTranslation";
-import { H2 } from "@components/Heading";
+import styles from './index.module.css'
+import projects from '@data/projects'
+import Card from '@components/Card'
+import JumboCard from '@components/JumboCard'
+import useTranslation from 'next-translate/useTranslation'
+import { H2 } from '@components/Heading'
 
-const ProjectsSection = () => {
-  const { t } = useTranslation();
+export default function ProjectsSection() {
+  const { t } = useTranslation()
 
-  const webs = projects.filter(
-    ({ category, active }) => category === "web" && active
-  );
-  const jumbo = webs.shift();
-  const apps = projects.filter(
-    ({ category, active }) => category === "app" && active
-  );
+  const activeProjects = projects.filter(({ active }) => active)
+
+  const webs = activeProjects.filter(({ category }) => category === 'web')
+  const jumbo = webs.shift()
+  const apps = activeProjects.filter(({ category }) => category === 'app')
 
   return (
     <section className={styles.container}>
       <section className={styles.secondarySection}>
-        <H2>
-          {t("common:projectsSectionWebTitle")}
-        </H2>
+        <H2>{t('common:projectsSectionWebTitle')}</H2>
         <JumboCard project={jumbo} />
         <ul className={styles.projectList}>
           {webs.map((project) => (
@@ -32,7 +28,7 @@ const ProjectsSection = () => {
       {apps.length !== 0 && (
         <section className={styles.secondarySection}>
           <h2 className={styles.sectionTitle}>
-            {t("common:projectsSectionAppTitle")}
+            {t('common:projectsSectionAppTitle')}
           </h2>
           {apps.length === 1 ? (
             <JumboCard project={apps[0]} />
@@ -46,7 +42,6 @@ const ProjectsSection = () => {
         </section>
       )}
     </section>
-  );
-};
+  )
+}
 
-export default ProjectsSection;
